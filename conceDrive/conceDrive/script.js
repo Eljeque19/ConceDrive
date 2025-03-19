6589c0415213b4149f03a8a248fec09a81b9383a
KO_Eljeque19 <cardonadaniel1905@gmail.com>
2025-03-19 10:24:52 -0500

    Add files via upload

---
+++ conceDrive/conceDrive/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuCatalogo = document.querySelector('.menu-catalogo');
+    const subMenu = menuCatalogo.querySelector('.submen');
+
+        subMenu.style.display = 'none';
+
+    menuCatalogo.addEventListener('mouseenter', function() {
+        subMenu.style.display = 'block';
+    });
+
+    menuCatalogo.addEventListener('mouseleave', function() {
+        subMenu.style.display = 'none'; 
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuCatalogo = document.querySelector('.menu-catalogo');
+    const subMenu = menuCatalogo.querySelector('.submen');
+
+    subMenu.style.display = 'none';
+
+    menuCatalogo.addEventListener('mouseenter', function() {
+        subMenu.style.display = 'block'; 
+    });
+
+    menuCatalogo.addEventListener('mouseleave', function() {
+        subMenu.style.display = 'none'; 
+    });
+
+    subMenu.addEventListener('mouseenter', function() {
+        subMenu.style.display = 'block'; 
+    });
+
+    subMenu.addEventListener('mouseleave', function() {
+        subMenu.style.display = 'none';
+    });
+});
